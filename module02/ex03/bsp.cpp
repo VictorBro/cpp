1e5f13c4c730c7774197ba97dd6097a19806150d
VictorBro <vmb1111@gmail.com>
2025-06-29 14:49:40 +0200

    Update bsp.cpp

--- module02/ex03/bsp.cpp
+++ module02/ex03/bsp.cpp
@@ -1,28 +1,5 @@
 #include "Point.hpp"
 
-// float area(Point const A, Point const B, Point const C)
-// {
-// 	float ret = (A.getX() * ((B.getY() - C.getY()))
-// 		+ B.getX() * (C.getY() - A.getY()) + C.getX() * (A.getY() - B.getY())).toFloat();
-// 	ret /= 2.0f;
-// 	return (ret < 0 ? -ret : ret);
-// }
-
-// bool bsp( Point const a, Point const b, Point const c, Point const point)
-// {
-//     float A1 = area(a, b, point);
-// 	//std::cout << "area size A1 is " << A1 << std::endl;
-// 	float A2 = area(b, c, point);
-// 	//std::cout << " area size A2 is " << A2 << std::endl;
-// 	float A3 = area(c, a, point);
-// 	//std::cout << "area size A3 is " << A3 << std::endl;
-// 	float BigT = area(a, b, c);
-// 	//std::cout << "area size BigT is " << BigT << std::endl;
-// 	std::cout << "A1 + A2 + A3 == BIGT ??\n"<< std::endl;
-// 	std::cout << A1 << " + "<< A2 << " + " << A3 << " == " << BigT << std::endl;
-// 	return (BigT == (A1 + A2 + A3));
-// }
-
 // Helper function to calculate the sign of triangle area
 // Positive if p is on left side of line ab, negative if on right side
 Fixed sign(Point const &p1, Point const &p2, Point const &p3)
